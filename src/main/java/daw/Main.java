1624611540d597c1fc53a881043b39c16b35f215
tgonati327 <tgonati327@g.educaand.es>
2024-01-16 12:34:37 +0100

    Creacion del proyecto. C1

---
+++ src/main/java/daw/Main.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package daw;
+
+/**
+ *
+ * @author tomas
+ */
+public class Main {
+    public static void main(String[] args) {
+        // Aqui ira el main del proyecto
+    }
+    
+}
