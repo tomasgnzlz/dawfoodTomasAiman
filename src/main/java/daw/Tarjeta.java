3bbd437517722175e9fbb2e52787ca099a93bb5d
Aiman <aimanfortnite2020@gmail.com>
2024-01-17 16:01:52 +0100

    C4: Clase tarjeta

--- src/main/java/daw/Tarjeta.java
+++ src/main/java/daw/Tarjeta.java
@@ -4,10 +4,82 @@
  */
 package daw;
 
+import java.time.LocalDate;
+
 /**
  *
  * @author aiman
  */
 public class Tarjeta {
+    private String nombreTitular;
+    private int numeroTarjeta;
+    private LocalDate fechaVencimiento;
+    private int CVV;
+    private double saldoTarjeta;
+
+    public Tarjeta(String nombreTitular, int numeroTarjeta, LocalDate fechaVencimiento, int CVV, double saldoTarjeta) {
+        this.nombreTitular = nombreTitular;
+        this.numeroTarjeta = numeroTarjeta;
+        this.fechaVencimiento = fechaVencimiento;
+        this.CVV = CVV;
+        this.saldoTarjeta = saldoTarjeta;
+    }
+
+    public Tarjeta() {
+    }
+    
+    public String getNombreTitular() {
+        return nombreTitular;
+    }
+
+    public int getNumeroTarjeta() {
+        return numeroTarjeta;
+    }
+
+    public LocalDate getFechaVencimiento() {
+        return fechaVencimiento;
+    }
+
+    public int getCVV() {
+        return CVV;
+    }
+
+    public double getSaldoTarjeta() {
+        return saldoTarjeta;
+    }
+    
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Tarjeta{");
+        sb.append("nombre del titular=").append(nombreTitular);
+        sb.append(", numero de la tarjeta=").append(numeroTarjeta);
+        sb.append(", fecha de vencimiento=").append(fechaVencimiento.getMonthValue()+"/"+fechaVencimiento.getYear());
+        sb.append(", CVV=").append(CVV);
+        sb.append(", saldo en la tarjeta=").append(saldoTarjeta);
+        sb.append('}');
+        return sb.toString();
+    }
+
+    public void setNombreTitular(String nombreTitular) {
+        this.nombreTitular = nombreTitular;
+    }
+
+    public void setNumeroTarjeta(int numeroTarjeta) {
+        this.numeroTarjeta = numeroTarjeta;
+    }
+
+    public void setFechaVencimiento(LocalDate fechaVencimiento) {
+        this.fechaVencimiento = fechaVencimiento;
+    }
+
+    public void setCVV(int CVV) {
+        this.CVV = CVV;
+    }
+
+    public void setSaldoTarjeta(double saldoTarjeta) {
+        this.saldoTarjeta = saldoTarjeta;
+    }
+    
     
 }
